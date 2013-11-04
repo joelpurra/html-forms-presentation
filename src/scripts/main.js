2041b2b4a156ab9170699b74f36f21d78bae7d7e
Joel Purra <code+github@joelpurra.com>
2013-11-04 11:28:01 +0100

    Basic presentation slides, styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
-bespoke.horizontal.from('article', {
-  bullets: 'li, .bullet',
-  scale: true,
-  hash: true,
-  progress: true,
-  state: true
-});
+(function() {
+    bespoke.horizontal.from("article", {
+        bullets: "ul:not(.no-bullets) li, ol:not(.no-bullets) li, .bullet",
+        scale: true,
+        hash: true,
+        progress: true,
+        state: true,
+        logbook: true
+    });
+}());
